7b6eb0d0448fd1519addc0d1d043156f41ddcadf
deritemakm <mikaellagonzales34@gmail.com>
2025-01-17 21:57:51 +0800

    change button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button1.Location = new System.Drawing.Point(513, 194);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(438, 169);
